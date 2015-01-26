257efa813c12e4b8a60a043877d98da1b3c813fe
kevllino <kevienid.eid@gmail.com>
2015-01-26 11:55:06 +0100

    New changes
    -listener
    -print and parse IPs

--- NetGraph/src/netgraph/NetGraph.java
+++ NetGraph/src/netgraph/NetGraph.java
@@ -4,8 +4,15 @@
  */
 package netgraph;
 
- import java.awt.*;
- import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.io.IOException;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.util.*;
+import java.lang.*;
+
 public class NetGraph extends JFrame {
 
     /**
@@ -15,24 +22,26 @@ public class NetGraph extends JFrame {
         // TODO code application logic here
          new NetGraph();
     }
-    private JTextField tZone = new JTextField();
+    
+private JTextField tZone = new JTextField();
     // Text area to display contents
 private JTextArea jta = new JTextArea();
+private String IP; 
+private String cmd ="tracert -d";
 
     public NetGraph(){
-        // Panel p to hold the label and text field
+        
+  // create GUI to enter IP and dispslay the graph
  JPanel p = new JPanel();
  p.setLayout(new BorderLayout());
  p.add(new JLabel("Enter IP or DN: "), BorderLayout.WEST);
  p.add(tZone, BorderLayout.CENTER);
  tZone.setHorizontalAlignment(JTextField.RIGHT);
-
  setLayout(new BorderLayout());
  add(p, BorderLayout.NORTH);
  add(new JScrollPane(jta), BorderLayout.CENTER);
-
-
-
+ 
+ tZone.addActionListener(new TextFieldListener());
  setTitle("IP");
  setSize(500, 300);
  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
@@ -40,10 +49,78 @@ private JTextArea jta = new JTextArea();
  
  System.out.println("Fenetre crée!");
  
- //Encore une autre 
- //Yo
- //cava et toi ? cool
- //blablbaablalb
- 
+
     }
+
+
+     //create the TEextListener to launch/execute the tracert command
+    private class TextFieldListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+            try{
+                //get the information from tZone
+                IP = tZone.getText().trim();
+                System.out.println(IP);
+                cmd += " "+IP;
+              
+                Runtime rt = Runtime.getRuntime();
+
+                Process proc = rt.exec(cmd);
+
+                BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+                
+                //parse output 
+                String[] check = parseOutput (stdInput);
+                
+                
+                
+            // read the output from the command
+           /* System.out.println("Here is the standard output of the command:\n");
+            String s = null;
+            while ((s = stdInput.readLine()) != null) {
+                    System.out.println(s);
+                    }*/
+            
+                
+            }
+           catch (Exception ex) {
+           System.err.println(ex);
+             }
+            
+        }
+        
+
+    }
+    
+    public String[] parseOutput (BufferedReader stdInput ){
+        String[] IP = null;
+         String s, s1 = null;
+         String[] tokens = null;
+         try{
+               //read each line
+            while ((s = stdInput.readLine()) != null) {
+                     //split each line into words
+                    tokens =  s.split("[ ]");
+                    for(int i=0; i< tokens.length; i++){
+                    System.out.print(tokens[i]+" ");
+                    if (tokens[i].matches("[0-9]*{1,3}\\.[0-9]*{1,3}\\.[0-9]*{1,3}\\.[0-9]*{1,3}")){
+                        IP[i] = tokens[i];
+                        
+                    }
+                    
+                }
+                    }
+           
+         }
+         catch(IOException e){
+               System.err.println(e);
+         }
+        
+        
+        return tokens;
+    }
+ 
 }
+
+
+

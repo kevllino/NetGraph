662b2bf809d51f5b396808420717f8a58c39174b
abedeini <armand.abedeini@gmail.com>
2015-01-31 11:36:38 +0100

    Jtree modif

--- NetGraph/src/netgraph/NetGraph.java
+++ NetGraph/src/netgraph/NetGraph.java
@@ -9,9 +9,16 @@ import java.awt.event.*;
 import java.io.*;
 import java.io.IOException;
 import javax.swing.*;
-import javax.swing.event.*;
-import java.util.*;
 import java.lang.*;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import netgraph.NetGraph;
 
 public class NetGraph extends JFrame {
 
@@ -22,15 +29,24 @@ public class NetGraph extends JFrame {
         // TODO code application logic here
          new NetGraph();
     }
-    
+ 
+private JTree tree;    
 private JTextField tZone = new JTextField();
     // Text area to display contents
-private JTextArea jta = new JTextArea();
+private JPanel jta = new JPanel();
 private String IP; 
 private String cmd ="tracert -d";
 
     public NetGraph(){
-        
+     //Tree
+      
+    DefaultMutableTreeNode top =
+        new DefaultMutableTreeNode("The Java Series");
+    createNodes(top);
+    tree = new JTree(top);
+   
+    JScrollPane treeView = new JScrollPane(tree);
+    //add(treeView, BorderLayout.CENTER);
   // create GUI to enter IP and dispslay the graph
  JPanel p = new JPanel();
  p.setLayout(new BorderLayout());
@@ -39,12 +55,15 @@ private String cmd ="tracert -d";
  tZone.setHorizontalAlignment(JTextField.RIGHT);
  setLayout(new BorderLayout());
  add(p, BorderLayout.NORTH);
- add(new JScrollPane(jta), BorderLayout.CENTER);
- 
+ jta.add(treeView);
+ add(jta, BorderLayout.CENTER);
+
  tZone.addActionListener(new TextFieldListener());
+ 
  setTitle("IP");
  setSize(500, 300);
  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+ 
  setVisible(true); // It is necessary to show the frame here!
  
  System.out.println("Fenetre crée!");
@@ -91,6 +110,42 @@ private String cmd ="tracert -d";
         
 
     }
+     private void createNodes(DefaultMutableTreeNode top) {
+    DefaultMutableTreeNode category = null;
+    DefaultMutableTreeNode book = null;
+    
+    category = new DefaultMutableTreeNode("Books for Java Programmers");
+    top.add(category);
+    
+    //original Tutorial
+    book = new DefaultMutableTreeNode("The Java Tutorial: A Short Course on the Basics");
+       
+    category.add(book);
+    
+    //Tutorial Continued
+    book = new DefaultMutableTreeNode("The Java Tutorial Continued: The Rest of the JDK");
+    category.add(book);
+    
+    //Swing Tutorial
+    book = new DefaultMutableTreeNode("The Swing Tutorial: A Guide to Constructing GUIs");
+        
+    category.add(book);
+
+    //...add more books for programmers...
+
+    category = new DefaultMutableTreeNode("Books for Java Implementers");
+    top.add(category);
+
+    //VM
+    book = new DefaultMutableTreeNode("The Java Virtual Machine Specification");
+         
+    category.add(book);
+
+    //Language Spec
+    book = new DefaultMutableTreeNode("The Java Language Specification");
+        
+    category.add(book);
+}
     
     public String[] parseOutput (BufferedReader stdInput ){
         String[] IP = null;
@@ -121,6 +176,3 @@ private String cmd ="tracert -d";
     }
  
 }
-
-
-
